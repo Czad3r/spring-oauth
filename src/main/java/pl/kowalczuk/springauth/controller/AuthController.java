365e19268d92adc0dc23ad58da953da4507c073f
czader98 <czader98@poczta.fm>
2019-11-20 20:53:30 +0100

    Initial oauth application, need to configure jdbc connection and oauth2 providers.
    Signed-off-by: czader98 <czader98@poczta.fm>

---
+++ src/main/java/pl/kowalczuk/springauth/controller/AuthController.java
@@ -0,0 +1,80 @@
+package pl.kowalczuk.springauth.controller;
+
+import pl.kowalczuk.springauth.exception.BadRequestException;
+import pl.kowalczuk.springauth.model.AuthProvider;
+import pl.kowalczuk.springauth.model.User;
+import pl.kowalczuk.springauth.payload.ApiResponse;
+import pl.kowalczuk.springauth.payload.AuthResponse;
+import pl.kowalczuk.springauth.payload.LoginRequest;
+import pl.kowalczuk.springauth.payload.SignUpRequest;
+import pl.kowalczuk.springauth.repository.UserRepository;
+import pl.kowalczuk.springauth.security.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = tokenProvider.createToken(authentication);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new BadRequestException("Email address already in use.");
+        }
+
+        User user = new User();
+        user.setName(signUpRequest.getName());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(signUpRequest.getPassword());
+        user.setProvider(AuthProvider.local);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/user/me")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "User registered successfully@"));
+    }
+}
---
+++ src/main/java/pl/kowalczuk/springauth/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package pl.kowalczuk.springauth.model;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google,
+    github
+}
